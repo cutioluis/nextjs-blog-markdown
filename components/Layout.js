6220668e6d691962af3785a3fbbb9839a2754d62
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2022-01-31 18:07:59 -0500

    fix layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <header className=" mb-8 py-4 flex justify-center">
-        <p className="text-center text-purple-300">Light Mode </p>
+        <Link href="/">
+          <a className="text-center text-purple-500">Dark Mode </a>
+        </Link>
         <button
           type="button"
-          class="bg-zinc-900 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg ml-5"
+          class="bg-purple-500 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg ml-5"
         ></button>
       </header>
 
@@ -15,9 +18,9 @@ const Layout = ({ children }) => {
 
       <footer className=" mt-8 py-4 text-gray-400 center">
         <div className="container mx-auto text-center mb-3 ">
-          You can see my work in <span className="text-white">Github</span> and{" "}
-          <span className="text-white">Behance</span> or see many tweets in my{" "}
-          <p className="text-white">Tweeter</p>
+          You can see my work in <span className="text-gray-600">Github</span>{" "}
+          and <span className="text-gray-600">Behance</span> or see many tweets
+          in my <p className="text-gray-600">Tweeter</p>
         </div>
       </footer>
     </div>
